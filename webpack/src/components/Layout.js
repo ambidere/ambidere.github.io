21430fc756f703527e25b9b6b145a780abc22e10
ambidere <xanthiandere@gmail.com>
2017-03-24 00:08:22 +0800

    configured webpack on github page

---
+++ webpack/src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<div id="container">
+			</div>
+		);
+	}
+}
